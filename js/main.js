196e5d3f3d8990c4b5e2eb7567732b8e9eb45e26
alan-garcia <alangarcianavarro@hotmail.com>
2020-12-23 16:30:27 +0100

    Refactorización general
    - Refactorización general.
    - Actualización de las dependencias de twitter bootstrap y sweetalert2.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,33 @@
+"use strict";
+
 const LOTTERY_TICKET_NUMBERS = 50;
 const LOTTERY_TICKET_STARS = 11;
 const SELECTION_NUMBERS_LIMIT = 5;
 const SELECTION_STARS_LIMIT = 2;
 
 let playButton = document.querySelector(".jugarButton");
+let instructionsButton = document.querySelector(".instruccionesButton");
+let numbersAndStarsIsLoaded = false;
+
+let numbersDiv = document.querySelector('.numeros');
+let numbersSelectedTicket = [];
+let number = 0;
+
+let starsDiv = document.querySelector('.estrellas');
+let starsSelectedTicket = [];
+let starsNumbers = 0;
+
+let goodLuckButton = document.querySelector(".botonSuerte");
+goodLuckButton.disabled = false;
+
 playButton.addEventListener("click", () => {
   document.querySelector(".jugar").classList.toggle("toggleButton");
-  loadNumbersAndStarsTicket();
+
+  if (!numbersAndStarsIsLoaded) {
+    loadNumbersAndStarsTicket();
+  }
 });
 
-let instructionsButton = document.querySelector(".instruccionesButton");
 instructionsButton.addEventListener("click", () => {
   document.querySelector(".instrucciones").classList.toggle("toggleButton");
 });
@@ -18,27 +36,46 @@ function loadNumbersAndStarsTicket() {
   for (let n = 1; n <= LOTTERY_TICKET_NUMBERS; n++) {
     numbersDiv.innerHTML += `<label><input type="checkbox" value='${n}' onchange="numbersSelection(this)"><span>${n}</span></label>`;
   }
-
   for (let stars = 1; stars <= LOTTERY_TICKET_STARS; stars++) {
     starsDiv.innerHTML += `<label><input type="checkbox" value='${stars}' onchange="starsSelection(this)"><span>${stars}</span></label>`;
   }
+
+  numbersAndStarsIsLoaded = true;
 }
 
-let numbersDiv = document.querySelector('.numeros');
-let numbersSelectedTicket = [];
-let number = 0;
-let isNumberFilled = false;
+function numbersSelection(thisNumber) {
+  if (thisNumber.checked) {
+    if (isSelectedAllNumbers(numbersSelectedTicket)) {
+      thisNumber.checked = false;
+      showWarningMessage("¡Cuidado!", `No puedes elegir más de ${SELECTION_NUMBERS_LIMIT} números. Para rectificar, haga click en un número ya seleccionado.`)
+    }
+    else {
+      numbersSelectedTicket.push(parseInt(thisNumber.value));
+    }
+  }
+  else {
+    let index = numbersSelectedTicket.indexOf(thisNumber.value);
+    numbersSelectedTicket.splice(index, 1);
+    thisNumber.checked = false;
+  }
+}
 
-let starsSelectedTicket = [];
-let starsDiv = document.querySelector('.estrellas');
-let starsNumbers = 0;
-let isStarsFilled = false;
-
-let elementsSelectedTicketArray = [];
-let element = 0;
-
-let goodLuckButton = document.querySelector(".botonSuerte");
-goodLuckButton.disabled = false;
+function starsSelection(thisNumber) {
+  if (thisNumber.checked) {
+    if (isSelectedAllStars(starsSelectedTicket)) {
+      thisNumber.checked = false;
+      showWarningMessage("¡Cuidado!", `No puedes elegir más de ${SELECTION_STARS_LIMIT} estrellas. Para rectificar, haga click en una estrella ya seleccionada.`);
+    }
+    else {
+      starsSelectedTicket.push(parseInt(thisNumber.value));
+    }
+  }
+  else {
+    let index = starsSelectedTicket.indexOf(thisNumber.value);
+    starsSelectedTicket.splice(index, 1);
+    thisNumber.checked = false;
+  }
+}
 
 function showWarningMessage(mytitle, message) {
   Swal.fire({
@@ -48,57 +85,16 @@ function showWarningMessage(mytitle, message) {
   });
 }
 
-function numbersSelection(thisNumber) {
-  if (thisNumber.checked) {
-    if (number === SELECTION_NUMBERS_LIMIT) {
-      thisNumber.checked = false;
-      number = SELECTION_NUMBERS_LIMIT;
-      showWarningMessage("¡Cuidado!", `No puedes elegir más de ${SELECTION_NUMBERS_LIMIT} números. Para rectificar, haga click en un número ya seleccionado.`)
-    }
-    else {
-      numbersSelectedTicket.push(parseInt(thisNumber.value));
-      number++;
-    }
-  }
-  else {
-    let index = numbersSelectedTicket.indexOf(thisNumber.value);
-    numbersSelectedTicket.splice(index, 1);
-    thisNumber.checked = false;
-    number--;
-  }
+function isSelectedAllNumbers(numbersSelectedTicket) {
+  return numbersSelectedTicket.length === SELECTION_NUMBERS_LIMIT;
 }
 
-function starsSelection(thisNumber) {
-  if (thisNumber.checked) {
-    if (starsNumbers === SELECTION_STARS_LIMIT) {
-      thisNumber.checked = false;
-      starsNumbers = SELECTION_STARS_LIMIT;
-      showWarningMessage("¡Cuidado!", `No puedes elegir más de ${SELECTION_STARS_LIMIT} estrellas. Para rectificar, haga click en una estrella ya seleccionada.`);
-    }
-    else {
-      starsSelectedTicket.push(parseInt(thisNumber.value));
-      starsNumbers++;
-    }
-  }
-  else {
-    let index = starsSelectedTicket.indexOf(thisNumber.value);
-    starsSelectedTicket.splice(index, 1);
-    thisNumber.checked = false;
-    starsNumbers--;
-  }
+function isSelectedAllStars(starsSelectedTicket) {
+  return starsSelectedTicket.length === SELECTION_STARS_LIMIT;
 }
-
-const orderNumbers = (a, b) => a - b;
 
 goodLuckButton.addEventListener("click", () => {
-  if (number === SELECTION_NUMBERS_LIMIT) {
-    isNumberFilled = true;
-  }
-  if (starsNumbers === SELECTION_STARS_LIMIT) {
-    isStarsFilled = true;
-  }
-
-  if (isNumberFilled && isStarsFilled) {
+  if (isSelectedAllNumbers(numbersSelectedTicket) && isSelectedAllStars(starsSelectedTicket)) {
     playEuromillon();
     goodLuckButton.disabled = true;
     document.querySelectorAll(".mensajeAciertos, .jugarEuromillon")
@@ -112,7 +108,6 @@ goodLuckButton.addEventListener("click", () => {
 function playEuromillon() {
   let myCombinationDiv = document.querySelector(".miCombinacion");
   let combinationWinnerDiv = document.querySelector(".combinacionGanadora");
-
   let getAllSuccessPrizesTableRow = document.querySelectorAll(".tablaPremios td:nth-child(2)");
 
   let myCombination = [];
@@ -122,7 +117,7 @@ function playEuromillon() {
   let successNumbersQuantity = 0;
   let successStarsQuantity = 0;
   let totalSuccess = "";
-  let index = 0;
+  var index = 0;
 
   getWinnerNumbers(numbersSelectedTicket, winnersNumbers, LOTTERY_TICKET_NUMBERS);
   getWinnerNumbers(starsSelectedTicket, winnersStars, LOTTERY_TICKET_STARS);
@@ -139,24 +134,22 @@ function playEuromillon() {
     } while(winnersArray.length < numbersSelectedArray.length); 
   }
 
+  const orderNumbers = (a, b) => a - b;
   numbersSelectedTicket.sort(orderNumbers);
   starsSelectedTicket.sort(orderNumbers);
   winnersNumbers.sort(orderNumbers);
   winnersStars.sort(orderNumbers);
 
-  myCombination = [...numbersSelectedTicket, ...starsSelectedTicket];
-  winnerCombination = [...winnersNumbers, ...winnersStars];
-
-  myCombination.splice(5, 0, " + ");
-  winnerCombination.splice(5, 0, " + ");
+  myCombination = [...numbersSelectedTicket, " + ", ...starsSelectedTicket];
+  winnerCombination = [...winnersNumbers, " + ", ...winnersStars];
 
   winnerCombination.forEach((winnerCombination, i) => {
     combinationWinnerDiv.innerHTML += winnerCombination + " ";
     myCombinationDiv.innerHTML += myCombination[i] + " ";
   });
 
-  successNumbersQuantity = winnersNumbers.filter(x => numbersSelectedTicket.includes(x)).length;
-  successStarsQuantity = winnersStars.filter(x => starsSelectedTicket.includes(x)).length;
+  successNumbersQuantity = winnersNumbers.filter(number => numbersSelectedTicket.includes(number)).length;
+  successStarsQuantity = winnersStars.filter(star => starsSelectedTicket.includes(star)).length;
 
   showSuccessPrizesTableRow();
 
@@ -173,14 +166,7 @@ function playEuromillon() {
   }
 
   document.querySelector(".resultado").style.display = "block";
-  
   document.querySelector(".reintentar").addEventListener("click", () => resetGame() );
-
-  function deselectNumbersAndStars() {
-    let numbersAndStars = document.querySelectorAll(".estrellas input, .numeros input");
-
-    numbersAndStars.forEach((currentValue) => currentValue.checked = false);
-  }
 
   function resetGame() {
     getAllSuccessPrizesTableRow[index].parentElement.classList.remove("acierto");
@@ -190,20 +176,13 @@ function playEuromillon() {
     deselectNumbersAndStars();
 
     numbersSelectedTicket.length = 0;
-    number = 0;
-    isNumberFilled = false;
-
     starsSelectedTicket.length = 0;
-    starsNumbers = 0;
-    isStarsFilled = false;
 
     myCombination.length = 0;
     winnerCombination.length = 0;
     winnersNumbers.length = 0;
     winnersStars.length = 0;
-    getAllSuccessPrizesTableRow.length = 0;
 
-    randomNumber = 0;
     successNumbersQuantity = 0;
     successStarsQuantity = 0;
     totalSuccess = "";
@@ -212,5 +191,10 @@ function playEuromillon() {
     myCombinationDiv.innerHTML = "";
 
     goodLuckButton.disabled = false;
+  }
+
+  function deselectNumbersAndStars() {
+    let numbersAndStars = document.querySelectorAll(".estrellas input, .numeros input");
+    numbersAndStars.forEach((currentValue) => currentValue.checked = false);
   }
 }
