25c687c58e7f14334bd2a2dd7c80aa394df877fe
alan-garcia <alangarcianavarro@outlook.com>
2024-05-24 12:17:21 +0200

    Introducción reglas del juego

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import logoEuromillones from '../src/img/logo-euromillones.png';
 import './App.css'
 
 function App() {
   return (
-    <>
-      
-    </>
+    <main id="euromillones">
+      <section id="euromillones-bienvenida">
+        <header id="euromillones-bienvenida-container">
+          <img src={logoEuromillones} alt="logo euromillones" />
+          <p className='euromillones-descripcion'>Bienvenido al juego del sorteo del <strong>Euromillones</strong> de la Lotería del Estado. Pulse en cualquiera de los 2 botones de abajo para comenzar a interactuar con la aplicación.</p>
+          <h2>¿Acepta el reto?</h2>
+          <div className="euromillones-acciones">
+            <button className="euromillones-acciones-btn euromillones-jugar-btn">JUGAR</button>
+            <button className="euromillones-acciones-btn euromillones-instrucciones-btn">INSTRUCCIONES</button>
+          </div>
+        </header>
+      </section>
+    </main>
   )
 }
 
